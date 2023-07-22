f308e4afbd59523cf2a061fed6214f5f9dd85451
QTruonggg <quangtruong102000@gmail.com>
2023-07-22 20:41:23 +0700

    new

--- DMAWS_T2203E_LuuQuangTruong/Models/Project.cs
+++ DMAWS_T2203E_LuuQuangTruong/Models/Project.cs
@@ -1,9 +1,17 @@
-﻿namespace DMAWS_T2203E_LuuQuangTruong.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DMAWS_T2203E_LuuQuangTruong.Models
 {
+    [Table("Projects")]
     public class Project
     {
+        [Key]
         public int ProjectId { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
         public string ProjectName { get; set; }
+        [Required]
         public DateTime ProjectStartDate { get; set; }
         public DateTime ProjectEndDate { get; set; }
         public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set; }
--- DMAWS_T2203E_LuuQuangTruong/Models/ProjectEmployee.cs
+++ DMAWS_T2203E_LuuQuangTruong/Models/ProjectEmployee.cs
@@ -1,9 +1,14 @@
-﻿namespace DMAWS_T2203E_LuuQuangTruong.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DMAWS_T2203E_LuuQuangTruong.Models
 {
+    [Table("ProjectEmployee")]
     public class ProjectEmployee
     {
         public int EmployeeId { get; set; }
         public int ProjectId { get; set; }
+        [Required]
         public string Tasks { get; set; }
         public virtual Employee Employees { get; set; }
         public virtual Project Projects { get; set; }
